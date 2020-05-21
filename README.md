8744f652d1998e8e1cb8a72dedf1417f9036a7c0
Osusara Kammalawatta <osusarak96@gmail.com>
2020-05-21 14:21:16 +0530

    some basics and a simple rest api

--- README.md
+++
@@ -1,5 +0,0 @@
-### Simple REST API
-
-This is a Simple REST API created using [Deno](https://deno.land/manual/introduction) to performe basic CRUD operations. To run the application,
-
-`deno run --allow-env --allow-net app.js`
---
+++ rest/server.ts
@@ -0,0 +1,12 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import router from "./routes.ts";
+
+const app = new Application();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+const port = 5000;
+console.log(`Server running on port: ${port}`);
+app.listen({ port });
+
